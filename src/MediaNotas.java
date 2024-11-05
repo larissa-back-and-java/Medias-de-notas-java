4962544ec09e86482f9e72a981e74b0cb5eb69ab
larissa <lary245ssa@gmail.com>
2024-11-05 14:41:39 -0300

    Adiciona projeto medias-de-notas no diretório media-de-notas

---
+++ src/MediaNotas.java
@@ -0,0 +1,29 @@
+  import java.util.Scanner;
+
+public class MediaNotas {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    double nota1, nota2, nota3, nota4, media;
+
+    System.out.print("Digite a primeira nota: ");
+    nota1 = scanner.nextDouble();
+
+    System.out.print("digite a segunda nota: ");
+    nota2 = scanner.nextDouble();
+
+    System.out.print("Digite a terceira nota: ");
+    nota3 = scanner.nextDouble();
+
+    System.out.print("Digite a quarta nota ");
+    nota4 = scanner.nextDouble();
+
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    System.out.printf("A média das nota é: %.2fn", media);
+
+    scanner.close();
+
+  }   
+}  
